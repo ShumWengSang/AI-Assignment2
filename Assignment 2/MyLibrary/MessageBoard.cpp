202f9d73cad7ef7299dbadbdc602cb6103b2346e
ShumWengSang <ballwallcallall@gmail.com>
2015-01-19 13:32:12 +0800

    MessageBoard First version.

--- Assignment 2/MyLibrary/MessageBoard.cpp
+++ Assignment 2/MyLibrary/MessageBoard.cpp
@@ -8,18 +8,36 @@ MessageBoard::~MessageBoard()
 {
 }
 
-void MessageBoard::sendMessage(string content, GameObject::GAMEOBJECT_TYPE type ) {
-	message = content;
-	if (type == GameObject::GO_CHIEF) {
-		//priority = 10;
-	}
+void MessageBoard::sendMessage(string content, GameObject::GAMEOBJECT_TYPE sender, GameObject::GAMEOBJECT_TYPE receiver, int piority )
+{
+	//
+	Message newMessage(content, sender, receiver, piority);
+
+	DebugMessage.NewMessage(newMessage);
+	themessages.push_back(newMessage);
 
 }
 
-void MessageBoard::getMessage()
+string MessageBoard::getMessage(GameObject::GAMEOBJECT_TYPE recv)
 {
-	//string * read = messages.back();
+	string theReturn = "";
+	std::vector<Message> theListofMessage;
 
+	//Put everything relevent into the ListofMessages
+	for(int i = 0; i < themessages.size(); i++)
+	{
+		if(themessages[i].receiver == recv)
+		{
+			theListofMessage.push_back(themessages[i]);
+		}
+	}
 
+	if(!theListofMessage.empty())
+	{
+		//Sort according to priority
+		std::sort(theListofMessage.begin(),theListofMessage.end());
+		theReturn = theListofMessage[0].message;
+	}
+	return theReturn;
 
 }
--- Assignment 2/MyLibrary/MessageBoard.h
+++ Assignment 2/MyLibrary/MessageBoard.h
@@ -2,22 +2,68 @@
 #include <string>
 #include <vector>
 #include "GameObject.h"
+#include <algorithm>
 using namespace std;
 
 #define HELP "Help";
 #define SHOOT "Shoot";
 #define TAKECOVER "Take Cover";
 
+struct Message
+{
+	Message(string Message, GameObject::GAMEOBJECT_TYPE sender, GameObject::GAMEOBJECT_TYPE receiver, int priority)
+	{
+		this->message = Message;
+		this->sender = sender;
+		this->receiver = receiver;
+		this->priority = priority;
+	}
+	unsigned int priority;
+	string message;
+	GameObject::GAMEOBJECT_TYPE sender;
+	GameObject::GAMEOBJECT_TYPE receiver;
+
+	bool operator< (const Message& rhs)
+	{
+		return this->priority < rhs.priority;
+	}
+
+	bool operator> (const Message& rhs)
+	{
+		return this->priority > rhs.priority;
+	}
+};
+
+class ShowMessages{
+public:
+	std::vector<Message> Messages;
+	void NewMessage(Message theMessage)
+	{
+		if(Messages.size() >= 10)
+		{
+			Messages.erase(Messages.begin());
+		}
+		Messages.push_back(theMessage);
+	}
+
+
+
+	void RenderMessageBoard()
+	{
+	}
+
+};
+
 class MessageBoard {
 public:
 	MessageBoard(); // Initialise to no message
 	~MessageBoard();
 
-	//int priority;
-	string message;
+	std::vector<Message> themessages;
 
-	std::vector<string *> messages;
-
-	void sendMessage(string content, GameObject::GAMEOBJECT_TYPE type);
-	void getMessage();
+	void sendMessage(string content, GameObject::GAMEOBJECT_TYPE sender, GameObject::GAMEOBJECT_TYPE receiver, int priority);
+	string getMessage(GameObject::GAMEOBJECT_TYPE recv);
+	
+	ShowMessages DebugMessage;
 };
+
